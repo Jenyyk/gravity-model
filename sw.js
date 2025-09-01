2e0e8f8904bfd7da10565d3ee168ad4c3a38974c
Jenyyk <jendotron2@gmail.com>
2025-09-01 22:17:26 +0200

    dev: make into PWA

---
+++ sw.js
@@ -0,0 +1,15 @@
+var GHPATH = 'gravity-model';
+
+var APP_PREFIX = 'jnk_';
+
+var VERSION = 'version_00';
+
+var URLS = [
+  `${GHPATH}/index.html`,
+  `${GHPATH}/styles.css`,
+  `${GHPATH}/script.js`,
+  `${GHPATH}/plotly.js`,
+  `${GHPATH}/bodyscript.js`,
+  `${GHPATH}/pkg/gravity_model.js`,
+  `${GHPATH}/pkg/gravity_model_bg.wasm`,
+];
